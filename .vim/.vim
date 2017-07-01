da8b5ef10e4fececd0db11d0be71e8c3e9d3447a
ebi-fry306.net <mailbox@ebi-fry306.net>
2017-07-01 21:38:56 +0900

    update

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/home/ebibi/dotfiles/.vim
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,4 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zsh ~/.zsh
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
